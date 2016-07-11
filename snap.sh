92695719522459f5cecc907a6635026cf10e7a29
luis.ramirez@opencloud.es <luis.ramirez@opencloud.es>
2016-07-11 12:10:30 +0200

    Mejoras snapsshots

--- snap.sh
+++ snap.sh
@@ -3,7 +3,7 @@
 #
 # Script para el tratamiento automatico de snapshots en un cluster Ceph
 # autor: Luis Ramirez - OpenCloud.es
-# version: 0.9a
+# version: 0.96a
 # fecha: 8/7/1026
 #
 ################################################################################
@@ -11,7 +11,56 @@ pools=(sata 10k ssd docout10k docoutssd)  # listado de pools a realizar snaps
 plog=/var/log/snaps/ # path del fichero de log
 rotacion=7 # periodo de retención en días
 fecharotado=`date '+%y.%m.%b' --date='-'$rotacion' day'`
-fecha=`datre '+%c'`
+fecha=`date '+%c'`
+
+# Función para la creación de snaps
+
+function mk-snap {
+                echo $fechalog "INFO - Generando Snapshot del objeto: "$2 >> $log
+                fecha=`date '+%y.%m.%d.%H.%M.%S'`
+                fechalog=`date '+%d/%b/%y %T'`
+                rbd snap create $1/$2@daily-$fecha
+                fechalog=`date '+%d/%b/%y %T'`
+                robjetos=""
+                robjetos=`rbd snap ls $1'/'$2 | grep $fecha`
+                z=0
+                for k in ${robjetos[@]}; do
+                        snapinfo[z]=$k
+                        z=$z+1
+                done
+                echo $fechalog "INFO -" $1/$2@daily-$fecha " SnapID: " ${snapinfo[0]}>> $log
+                echo $fechalog "INFO - Fin Snapshot del objeto: "$2 >> $log
+           }
+
+# Funcion para el volcado de las snaps activas para un objeto determinado
+
+function ls-snaps {
+                dlogobj=$plog$1'/'
+                flogobj=$dlogobj$2'.log'
+                fecha=`date '+%y.%m.%d.%H.%M.%S'`
+                tlogobj=$dlobobj'-'$feha'.tgz'
+                [ -d $dlogobj ] || mkdir $dlogobj
+                tar -cvzf $tlogobj $flobobj
+                rm -rf $flogobj
+                rbd snap ls $1'/'$2 >> $flogobj
+           }
+
+# Funcionn para el rotado de snaps
+
+function rt-sanps {
+                robjetos=`rbd snap ls $1'/'$2 | grep $fecharotado`
+                z=0
+                for k in ${robjetos[@]}; do
+                        snapinfo[z]=$k
+                        z=$z+1
+                done
+                fechalog=`date '+%d/%b/%y %T'`
+                echo $fechalog "INFO - Rotado de snap: "$2 >> $log
+                rbd snap rm $1/$2@${snapinfo[1]}
+                fechalog=`date '+%d/%b/%y %T'`
+                echo $fechalog "INFO - Borrado de snap id: ${snapinfo[0]} en "$2 >> $log
+           }
+
 # recorremos todos los pools
 echo "############################################################"
 echo "Snapshots de los pools a fecha... " $fecha
@@ -25,35 +74,14 @@ for i in ${pools[@]}; do
         echo $fechalog "############################################################" >> $log
         for j in ${objetos[@]}; do
             # Creación de snaps
-                echo $fechalog "INFO - Generando Snapshot del objeto: "$j >> $log
-                fecha=`date '+%y.%m.%d.%H.%M.%S'`
-                fechalog=`date '+%d/%b/%y %T'`
-                rbd snap create $i/$j@daily-$fecha
-                fechalog=`date '+%d/%b/%y %T'`
-                robjetos=""
-                robjetos=`rbd snap ls $i'/'$j | grep $fecha`
-                z=0
-                for k in ${robjetos[@]}; do
-                        snapinfo[z]=$k
-                        z=$z+1
-                done
-                echo $fechalog "INFO -" $i/$j@daily-$fecha " SnapID: " ${snapinfo[0]}>> $log
-                echo $fechalog "INFO - Fin Snapshot del objeto: "$j >> $log
+                mk-snap $i $j
             # Fin creación snaps
-            # Rotado de snaps
-                fecharotado=8716
-                robjetos=`rbd snap ls $i'/'$j | grep $fecharotado`
-                z=0
-                for k in ${robjetos[@]}; do
-                        snapinfo[z]=$k
-                        z=$z+1
-                done
-                fechalog=`date '+%d/%b/%y %T'`
-                echo $fechalog "INFO - Rotado de snap: "$j >> $log
-                rbd snap rm $i/$j@${snapinfo[1]}
-		fechalog=`date '+%d/%b/%y %T'`
-                echo $fechalog "INFO - Borrado de snap id: ${snapinfo[0]} en "$j >> $log
+            # Rotado de Snaps
+        #       rt-snaps $i $j
             # Fin Rotado de snaps
+            # Almaenamos log con las snaps actuales
+                ls-snaps $i $j
+            # Finn log snaps actuales
         done
         echo $fechalog "############################################################" >> $log
         echo $fechalog "#           INFO - Fin $i  $fechalog            " >> $log
